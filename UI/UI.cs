ef6f5ac74488a7404aa8595038b32781b992746d
Aleksandr <evdokimov.aleksandr.2004@gmail.com>
2024-12-01 16:28:59 +0300

    Defend of Lab added

--- UI/UI.cs
+++ UI/UI.cs
@@ -5,37 +5,47 @@ namespace UI
     {
         public static void Main()
         {
-            try
-            {
-                Student student = new(4, "Никита", "Аверин", "НД", "НД", "НД");
-                Student student2 = new(0, "Александр", "Владимирович", "Евдокимов", "НД", "НД", "НД");
-                Student student3 = new(1, "Владислав", "Бекренёв", "НД", "НД", "НД");
-                Student student4 = new(2, "Артём", "Андреевич", "Блохин", "НД", "НД", "НД");
-                Student student5 = new(3, "Михаил", "Коньков", "НД", "НД", "НД");
-
+            
                 Group group = new("245");
-                group.AddStudent(student);
-                group.AddStudent(student2);
-                group.AddStudent(student3);
-                group.AddStudent(student4);
-                group.AddStudent(student5);
-
-                group[3]=new("Ирина", "Кочеткова", "НД", "НД", "НД");
-                group[3].PhoneNumber = "8 800 555 35 35";
-
-                group.RemoveStudent(0);
-
-                group.RemoveStudent(student);
-
-                Console.WriteLine("Студент с номером зачётной книжки 3: \n" + group[3]);
-
-                Console.Write(group.GetInfo());
-
-                group.RemoveStudent(student2);
-            }
-            catch (Exception e) { 
+                
+                bool isRunning = true;
+                int currentAddedIndex = 0;
+                while (isRunning)
+                {
+                try
+                {
+                    Console.Write("Введите команду:\n0 - выход\n1 - добавить студента\n2 - удалить студента\n3 - посмотреть студентов\n");
+                    int code = Convert.ToInt32(Console.ReadLine());
+                    switch (code)
+                    {
+                        case 0: isRunning = false; break;
+                        case 1:
+                            Console.WriteLine("Введите имя");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Введите фамилию");
+                            string sourname = Console.ReadLine();
+                            
+                            group.AddStudent(new Student(currentAddedIndex, name, sourname, "неизвестно", "неизвестно", "неизвестно"));
+                            currentAddedIndex++;
+                            break;
+                        case 2:
+                            Console.WriteLine("Введите номер студента");
+                            group.RemoveStudent(Convert.ToInt32(Console.ReadLine()));
+                            break;
+                        case 3:
+                            Console.WriteLine(group);
+                            break;
+                        default:
+                            Console.WriteLine("Нет такой команды");
+                            break;
+                    }
+                }
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
             }
+        }
+           
         }
     }
 }
